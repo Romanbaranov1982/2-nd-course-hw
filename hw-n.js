1e152de58df6ee52bf913b2746f0b9259886803d
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-02-17 11:21:36 +0300

    JS training

---
+++ hw-n.js
@@ -0,0 +1,22 @@
+
+// let a = 10;// Задание 1
+// alert("10");
+// let b = 20;
+//  alert("20"); 
+
+//  let firstiPhoneYear = 2007 ; Задание 2
+//  alert(firstiPhoneYear);
+//  alert("Год выхода первого iPhone ${firstiPhoneYear}" );
+
+/* 
+let creatorJS = "Brendan Eich" ;//Задание 3
+alert(creatorJS);
+*/
+/* задание 4
+let a = 2;
+let b = 10;
+alert(a+b);// сумма
+alert(b-a);//разность
+alert(a*b);//произведение
+alert(b/a);//деление
+*/
