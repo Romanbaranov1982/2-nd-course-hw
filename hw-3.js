3075bf18e73e27ef62cff302348cf72cf763bc84
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-02-22 18:47:38 +0300

    задание 1

---
+++ hw-3.js
@@ -0,0 +1,9 @@
+/*
+let password = 'Michael_Jordan';//  Задание 1
+let enter = prompt('Введите пароль');
+if (password = enter) {
+    alert('Пароль введен верно');
+} else {
+    alert('Пароль введен неправильно');
+}
+*/
