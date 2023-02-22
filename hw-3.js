4eee2b1a4c3c9dddb4f844010be4c3a60cbd305d
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-02-22 21:14:40 +0300

    задание 5

--- hw-3.js
+++ hw-3.js
@@ -24,3 +24,52 @@ if (d>100 || e>100) {
     alert('Неверно');
 }
 */
+/*
+let a = 2;// задание 4
+let b = 3;
+alert(a + b);
+*/
+/*
+let monthNumber = Number(prompt('Введите номер месяца')) ;//задание 5
+switch (monthNumber) {
+    case 1  :
+        alert('Зима');
+        break;
+        case 2  :
+        alert('Зима');
+        break;
+        case 3  :
+        alert('Весна');
+        break;
+        case 4  :
+        alert('Весна');
+        break;
+        case 5  :
+        alert('Весна');
+        break;
+        case 6  :
+            alert('Лето');
+            break;
+            case 7  :
+                alert('Лето');
+                break;
+                case 8  :
+            alert('Лето');
+            break;
+            case 9  :
+                alert('Осень');
+                break;
+                case 10  :
+                    alert('Осень');
+                    break;
+                    case 11  :
+                        alert('Осень');
+                        break;
+                        case 12  :
+                            alert('Зима');
+                            break;                    
+
+    default:
+        break;
+}
+*/
