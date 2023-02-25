544614b4663057721b2c86b4fd0c2c5ad0fd3ae4
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-02-25 15:24:06 +0300

    восстановдение утерянного

---
+++ hw-4.js
@@ -0,0 +1,48 @@
+
+
+// let talk = 1;// задание 1
+// while (talk<3) {
+//     console.log('Привет !');
+//     talk++;
+// }
+
+// let i = 1; //задание  2
+
+// while (i < 6) {
+// 	console.log(i);
+// 	i++;
+// }
+
+// let i = 7; задание 3
+
+// while (i < 23) {
+// 	console.log(i);
+// 	i++;
+// }
+
+// const mens =  {
+//     'Коля': 200,
+//     'Петя': 300,
+//     'Вася': 400
+// };// задание 4
+// for (const name in mens) {
+//     console.log(`${name} - зарплата ${mens[name]} долларов`);
+// }
+
+/*
+let n = 1000;// задание 5
+let num = 5;
+do {   
+console.log(n) ;
+ n/=2; 
+ } while (n>50);
+ console.log(`Деление выполняется ${num} раз`);
+*/
+
+//  let friday = 1;// задание 6
+// do {
+//     if (friday % 7 == 1) {
+//         console.log(`Сегодня пятница, ${friday}-е число. Необходимо подготовить отчет.`);
+//     }    
+//     friday++ ;
+// } while ( friday<=31);
