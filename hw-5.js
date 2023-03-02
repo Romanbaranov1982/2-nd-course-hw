8bd47d86f20de089ea87d9a92e82f0f665974365
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-03-02 12:47:02 +0300

    задание 2 и 3

--- hw-5.js
+++ hw-5.js
@@ -9,3 +9,25 @@
 //     }
 // }
 // comparison(7, 2);
+
+// function evenOdd(n) {  //задание 2
+//     if (n % 2 == 0) {
+//         alert('Число четное')
+//     } else {
+//         alert('Число нечетное')
+//     }
+    
+// }
+// evenOdd(2);
+
+// let enyNumb = function degree(n) { // задание 3.1
+//     return n**2;
+//     console.log(n);    
+// }
+// enyNumb(5);
+
+// let enyNumb = function degree(n) {// задание 3.2
+//     return n**2;
+        
+// }
+// console.log(enyNumb(7));
