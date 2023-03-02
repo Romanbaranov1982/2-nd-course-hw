e8e28db7770e3b0776a88d382f14fa942eb73fa9
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-03-02 11:42:38 +0300

    задание 1

---
+++ hw-5.js
@@ -0,0 +1,11 @@
+// function comparison(a, b) { // задание 1
+//     if (a > b) {
+//        alert(a); 
+//     } else if (b > a) {
+//         alert(b);
+//     }
+//     else if (a === b) {
+//         alert(a);
+//     }
+// }
+// comparison(7, 2);
