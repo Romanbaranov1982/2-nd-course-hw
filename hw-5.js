4bf777ec39bb57443b5e5fb788bda992fe898729
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-03-03 10:37:12 +0300

    13

--- hw-5.js
+++ hw-5.js
@@ -68,6 +68,68 @@
 // }
 // somethinck(num);
 
+// function season() {
+    //     let monthNumber = prompt('Введите номер месяца');
+    //     if (isNaN(monthNumber) || monthNumber > 13 || monthNumber <= 0) {
+    //         alert('Введено некорректное значение')
+    //     } else if  (monthNumber === 12 || monthNumber <= 2) {
+    //         alert ('Зима');
+    //     } else if (monthNumber >=3 && monthNumber <= 5) {
+    //         alert('Весна');                           // Задание 7
+    //     } else if (monthNumber >= 6 && monthNumber <= 8) {
+    //         alert('Лето');
+    //     } else if  (monthNumber >=9 && monthNumber <= 11) {
+    //         alert('Осень');
+    //     }
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function season() {
 //     let monthNumber = prompt('Введите номер месяца');
