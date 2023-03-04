2ba61c467f3ebd5111209bb91f2df3919820d027
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-03-04 22:57:29 +0300

    задание 6

--- hw-6.js
+++ hw-6.js
@@ -15,3 +15,13 @@
 //  let numbs = [1, 3, 5, 10, 20];// Задание 3
 //  numbs = numbs.join(' ');
 //  console.log(numbs);
+
+// let arrow = [1, 1, 1]; //Задание 5
+// arrow.push(2, 2, 2);
+// console.log(arrow);
+
+// let arr = [9, 8, 7, 'a', 6, 5];// задание 6
+// arr = arr.sort();
+// console.log(arr);
+// console.log(arr.pop());
+// console.log(arr);
