b92cb3fb2196d7e2f6779ec1b152bba9f0cea86b
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-03-04 21:22:25 +0300

    Задание  2 и 3

--- hw-6.js
+++ hw-6.js
@@ -4,3 +4,14 @@
 // 	if (numbs[i] == 10) break;
 // 	console.log(numbs[i]);
 // }
+ 
+// const numbs = [1, 5, 4, 10, 0, 3];// Задание 2
+
+// numbs.forEach((el , index) => {
+//     if (el === 4) {console.log(`${index}`);
+// }
+	
+// });
+//  let numbs = [1, 3, 5, 10, 20];// Задание 3
+//  numbs = numbs.join(' ');
+//  console.log(numbs);
