905727564d424df9311bc20291801c9f554959ce
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-03-08 17:39:55 +0300

    222

--- hw-7.js
+++ hw-7.js
@@ -1,2 +1,16 @@
 // let str = 'js';// задание 1
 // console.log(str.toUpperCase());
+
+let arr = ['бобёр', 'Боб', 'большой', 'Борис',
+'Бобби'];
+let coincidence = 'боб';
+
+let searchStart = (arr, coincidence) => {
+    if (arr.toLowerCase().startsWith(coincidence.toLowerCase())){
+        console.log(arr);
+    }
+};
+
+ searchStart();
+
+
