63c8c1697e4bb2711edfac60635975393e7bc010
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-03-08 21:09:25 +0300

    задание4

--- hw-7.js
+++ hw-7.js
@@ -1,16 +1,25 @@
 // let str = 'js';// задание 1
 // console.log(str.toUpperCase());
 
-let arr = ['бобёр', 'Боб', 'большой', 'Борис',
-'Бобби'];
-let coincidence = 'боб';
+// let a = ['бобёр', 'Боб', 'большой', 'Борис',
+// 'Бобби'];
+// let b = 'боб';
 
-let searchStart = (arr, coincidence) => {
-    if (arr.toLowerCase().startsWith(coincidence.toLowerCase())){
-        console.log(arr);
-    }
-};
 
- searchStart();
+// let searchStart = (newArr) => {
+//     if (a.toLowerCase().startsWith(b.toLowerCase())) {
+//       console.log(newArr);
+//     }        
+//     };
+// searchStart(); 
+
+// let numb = 32.58884 ;// задание 3
+// console.log(Math.ceil(numb));
+// console.log(Math.floor(numb));
+// console.log(Math.round(numb, 1));
+
+// let numArray = [52, 53, 49, 77, 21, 32];  // задание4
+// console.log(Math.max(...numArray));/// максимальное значение
+// console.log(Math.min(...numArray));// минимальное
 
 
