483ecf7e827640e6dd8588b69270219a4fe486ff
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-03-09 16:17:38 +0300

    задача 7 и 6

--- hw-7.js
+++ hw-7.js
@@ -4,10 +4,11 @@
 // let a = ['бобёр', 'Боб', 'большой', 'Борис',
 // 'Бобби'];
 // let b = 'боб';
+// let newArr = [];
 
-
-// let searchStart = (newArr) => {
-//     if (a.toLowerCase().startsWith(b.toLowerCase())) {
+// let searchStart = (a, b) => {
+//     if (a.toLowerCase().startsWith(b.toLowerCase() || a.filter() === String)) {
+// newArr.push(searchStart);
 //       console.log(newArr);
 //     }        
 //     };
@@ -21,5 +22,32 @@
 // let numArray = [52, 53, 49, 77, 21, 32];  // задание4
 // console.log(Math.max(...numArray));/// максимальное значение
 // console.log(Math.min(...numArray));// минимальное
+ 
+// function rundomNumber(min, max) {// задание 5
+//     min = Math.ceil(1);
+//     max = Math.floor(10);
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// console.log(rundomNumber());
 
+// let inNumb = prompt('Ведите любое число'); задача 6
+// let arrayN = [];
+// let getRandomArrNumbers  = (i) => {
+//        do {
+//         let i = Math.round((Math.random() * inNumb));
+//        arrayN.push(i);
+//        } while (arrayN.length <= (inNumb / 2) - 1);// почему работает только с ''-1'' ???
+// };
+// getRandomArrNumbers();
+// console.log(arrayN);
+
+// let min = prompt('Введите любое число');// задача 7
+// let max = prompt('Введите любое число');
+
+// let randomNumb = (min, max) => {
+//     min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
+// console.log(randomNumb(min, max));
 
