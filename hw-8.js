a1fbee9dff5ec32d82d57758d24f9b9748de7dfb
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-03-11 20:25:46 +0300

    1.2

--- hw-8.js
+++ hw-8.js
@@ -1,7 +1,13 @@
-let arr = [1, 3, 5, 7, 9];
-let mult = arr.reduce((a, b) => a * b );
-let sum = arr.reduce((a, b) => a + b );
-function result(mult) {    
-console.log(mult);
-} 
-result(mult); 
+// let arr = [1, 3, 5, 7, 9]; // задание 1
+// let mult = arr.reduce((a, b) => a * b );
+// let sum = arr.reduce((a, b) => a + b );
+// let callback;
+// function result(callback) {  
+//     if (callback === mult) {
+//         console.log(mult);
+//     }  else if (callback === sum) {
+// console.log(sum);
+//     }
+
+// } ;
+// result(mult, arr); 
